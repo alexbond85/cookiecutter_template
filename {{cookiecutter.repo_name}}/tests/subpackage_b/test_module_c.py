26897b323c7e84383c9a6ceefcad2ae72f34f524
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-07-12 17:23:16 +0200

    complete cookiecutter template

---
+++ {{cookiecutter.repo_name}}/tests/subpackage_b/test_module_c.py
@@ -0,0 +1,6 @@
+from {{cookiecutter.package_name}}.subpackage_b.module_c import number_of_lines
+
+
+def test_number_of_lines(data_directory, max_lines):
+    filepath = data_directory.joinpath("arrivals.csv")
+    assert number_of_lines(filepath) <= max_lines
